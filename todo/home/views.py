f056595e42f9aef369b6dc8b312cf38f5d94b83b
jahedhasan <jahedhasan.bs@gmail.com>
2025-03-21 05:44:32 +0600

    showing data from db- get to template

--- todo/home/views.py
+++ todo/home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render
+from .models import *
+
+
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        data = {
+            'todos' : TODO.objects.all()
+        }
+        return render(request, 'home.html', context=data)
