18e2f5b319fbb7ebc9e61fea16b31d3a952670c3
jahedhasan <jahedhasan.bs@gmail.com>
2025-04-13 00:55:00 +0600

    added python basic

---
+++ basic-python/practice.py
@@ -0,0 +1,55 @@
+# variable
+x = 10
+name = "Jahed"
+is_active = True
+# Python Data Types
+
+
+# 1. Integers
+
+x = 10
+y = -5
+
+
+
+# 2. Floats
+
+pi = 3.14
+temperature = -5.6
+
+# 3. Strings
+
+name = "Jahed"
+greeting = 'Hello, world!'
+
+
+# 4. Booleans
+
+is_active = True
+is_finished = False
+
+
+# 5. Lists
+
+fruits = ["apple", "banana", "cherry"]
+numbers = [1, 2, 3, 4]
+
+
+# 6. Dictionaries
+
+student = {"name": "Jahed", "age": 22}
+person = {"first_name": "John", "last_name": "Doe"}
+
+# 7. Tuples
+
+coordinates = (10, 20)
+dimensions = (5, 10, 15)
+
+# 8. Sets
+
+colors = {"red", "green", "blue"}
+numbers = {1, 2, 3, 4}
+
+
+# 9. None
+value = None
